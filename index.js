addec3ff15795ef7b10965c4d724223a8fc2e1b5
Mark Gardner <mjg@phoenixtrap.com>
2019-02-18 13:42:45 -0600

    read word list

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 var Word = require("./Word");
+var inquirer = require("inquirer");
+var fs = require("fs");
+
+fs.readFile("./words.json", "utf8", function(error, data) {
+  if (error) throw error;
+  var wordList = JSON.parse(data);
+  console.log(wordList);
+});
